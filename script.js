db7e5d71c1412cfd78b07d2e5c364f875afeb3a9
Ola Al Jarrah <66041474+ola511@users.noreply.github.com>
2021-12-29 11:51:55 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function changeColor() {
+    document.getElementById("change").style.color = "green";
+    return false;
+ }
+
+ function changeBkg() {
+    document.body.style.backgroundColor = "pink";
+    return false;
+ }
+
